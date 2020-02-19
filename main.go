0d5a288d795aaabbf2e274d8764e7a5a3146f8e6
Alonecmj <duanfei.job@gmail.com>
2020-02-19 17:05:06 +0800

    master commit

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
+/*
+ * @Description:
+ * @Author: violeteve
+ * @Date: 2020-02-19 16:04:02
+ * @LastEditors: violeteve
+ * @LastEditTime: 2020-02-19 17:04:29
+ * @FilePath: /go-github-actions/main.go
+ */
 package main
 
 import (
 	"fmt"
 
-	"github.com/cnych/go-github-actions/hello"
+	"github.com/duangfei/go-github-actions/hello"
 )
 
 func main() {
