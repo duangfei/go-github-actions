a751ae9d2bed3034c372c838e5bf546427077fe5
Alonecmj <duanfei.job@gmail.com>
2020-02-19 17:10:35 +0800

    master commit

--- main.go
+++ main.go
@@ -1,11 +1,3 @@
-/*
- * @Description:
- * @Author: violeteve
- * @Date: 2020-02-19 16:04:02
- * @LastEditors: violeteve
- * @LastEditTime: 2020-02-19 17:04:29
- * @FilePath: /go-github-actions/main.go
- */
 package main
 
 import (
